00891f6c389ad5bd8b2001df2b8e8853cbf20a27
Finn-Perera <151525130+Finn-Perera@users.noreply.github.com>
2024-11-24 16:22:01 +0000

    Updated hex library to get directional vectors as an index

--- src/main/java/io/github/finnperera/playmodular/initialframework/Hex.java
+++ src/main/java/io/github/finnperera/playmodular/initialframework/Hex.java
@@ -1,55 +1,85 @@
 package io.github.finnperera.playmodular.initialframework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * This class was created mostly from this blog:
  * <a href="https://www.redblobgames.com/grids/hexagons/implementation.html">...</a>
  * Which outlines the majority of functions a hexagonal engine requires.
  */
 public class Hex {
-    private int q, r, s;
     private static final Hex[] hexDirections =
-            new Hex[]{new Hex(1, 0, -1), new Hex(1, -1,0), new Hex(0, -1, 1),
+            new Hex[]{new Hex(1, 0, -1), new Hex(1, -1, 0), new Hex(0, -1, 1),
                     new Hex(-1, 0, 1), new Hex(-1, 1, 0), new Hex(0, 1, -1)};
+    private int q, r, s;
+
 
     public Hex(int q, int r, int s) { // for cube
-        assert(q + r + s == 0); // ensures valid coordinates
+        assert (q + r + s == 0); // ensures valid coordinates
         this.q = q;
         this.r = r;
         this.s = s;
     }
 
-    public boolean equals(Hex h) {
-        return q == h.q && r == h.r && s == h.s;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Hex hex = (Hex) obj;
+        return q == hex.q && r == hex.r && s == hex.s;
     }
 
-    public Hex hexAdd(Hex a, Hex b) {
+    @Override
+    public int hashCode() {
+        return Objects.hash(q, r);
+    }
+
+    public static Hex hexAdd(Hex a, Hex b) {
         return new Hex(a.q + b.q, a.r + b.r, a.s + b.s);
     }
 
-    public Hex hexSubtract(Hex a, Hex b) {
+    public static Hex hexSubtract(Hex a, Hex b) {
         return new Hex(a.q - b.q, a.r - b.r, a.s - b.s);
     }
 
-    public Hex hexMultiply(Hex a, int k) {
+    public static Hex hexMultiply(Hex a, int k) {
         return new Hex(a.q * k, a.r * k, a.s * k);
     }
 
-    public int hexLength(Hex hex) {
+    public static int hexLength(Hex hex) {
         return (Math.abs(hex.q) + Math.abs(hex.r) + Math.abs(hex.s)) / 2;
     }
 
-    public int hexDistance(Hex a, Hex b) {
+    public static int hexDistance(Hex a, Hex b) {
         return hexLength(hexSubtract(a, b));
     }
 
+    public static int hexDirectionAsIndex(Hex hex) {
+        for (int i = 0; i < hexDirections.length; i++) {
+            if (hexDirections[i].equals(hex)) return i;
+        }
+        assert false : "Hex direction is invalid" + hex;
+        return -1;
+    }
+
     // must be 0 to 5
-    public Hex hexDirection(int direction) {
-        assert(direction >= 0 && direction < 6);
+    public static Hex hexDirection(int direction) {
+        assert (direction >= 0 && direction < 6);
         return hexDirections[direction];
     }
 
-    public Hex hexNeighbour(Hex hex, int direction) {
+    public static Hex hexNeighbour(Hex hex, int direction) {
         return hexAdd(hex, hexDirection(direction));
+    }
+
+    public static List<Hex> getNeighbours(Hex hex) {
+        List<Hex> neighbours = new ArrayList<>();
+        for (int i = 0; i < hexDirections.length; i++) {
+            neighbours.add(hexNeighbour(hex, i));
+        }
+        return neighbours;
     }
 
 }
