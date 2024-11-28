596ec6f06f25a92f7d46ab176b244a9895ed0e97
Finn-Perera <151525130+Finn-Perera@users.noreply.github.com>
2024-11-28 13:11:41 +0000

    Expanded HiveMove and Move classes

--- src/main/java/io/github/finnperera/playmodular/initialframework/HiveMove.java
+++ src/main/java/io/github/finnperera/playmodular/initialframework/HiveMove.java
@@ -1,8 +1,26 @@
 package io.github.finnperera.playmodular.initialframework;
 
 public class HiveMove extends Move {
-    public HiveMove(HiveTile tileToMove, Hex nextPosition) {
+
+    private boolean placementMove;
+
+    public HiveMove(HiveTile tileToMove, Hex nextPosition, boolean placementMove) {
         this.pieceToMove = tileToMove;
         this.nextPosition = nextPosition;
+        this.placementMove = placementMove;
+    }
+
+    @Override
+    public HiveTile getPieceToMove() {
+        return (HiveTile) super.getPieceToMove();
+    }
+
+    @Override
+    public Hex getNextPosition() {
+        return (Hex) super.getNextPosition();
+    }
+
+    public boolean isPlacementMove() {
+        return placementMove;
     }
 }
--- src/main/java/io/github/finnperera/playmodular/initialframework/Move.java
+++ src/main/java/io/github/finnperera/playmodular/initialframework/Move.java
@@ -1,4 +1,15 @@
 package io.github.finnperera.playmodular.initialframework;
 
+// May want to make this generic
 public abstract class Move {
+    protected Piece pieceToMove;
+    protected Position nextPosition;
+
+    public Piece getPieceToMove() {
+        return pieceToMove;
+    }
+
+    public Position getNextPosition() {
+        return nextPosition;
+    }
 }
